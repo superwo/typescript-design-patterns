5b6bd9e6c83f8bc5bfd7625e7f5768ef694b778a
Radu <radu.avadanii@gmail.com>
2026-02-02 15:25:46 +0200

    init: first app.

--- index.ts
+++ index.ts
@@ -1,48 +1,38 @@
-// мы можем использовать typescript типы для создания рекурсивных структур
+type TodoList = [
+    {
+        id: 1;
+        text: "Create something";
+        completed: false;
+    },
+];
 
-type Tree<G> = {
-    value: G;
-    children: Tree<G>[];
-};
+const ex: TodoList = [
+    {
+        id: 1,
+        text: "Create something",
+        completed: false,
+    },
+];
 
-const tree: Tree<number> = {
-    value: 1,
-    children: [{ value: 2, children: [] }],
-};
-
-// Мы можем это использовать для рекурсивных дженериков
-
-type MyAwaited<T> = T extends Promise<infer V> ? MyAwaited<V> : T;
-
-type Res1 = MyAwaited<Promise<Promise<Promise<number>>>>;
-
-// И для рекурсивного обхода вложенных объектов
-
-type DeepReadonly<T> = {
-    readonly [K in keyof T]: DeepReadonly<T[K]>;
-};
-
-type Res2 = DeepReadonly<{ value: { title: string } }>;
-
-// Но мы можем использовать это для содания конкретных таких циклов!
-
-type Reverse<Tuple extends unknown[]> = Tuple extends [
+type AddItem<TList extends TodoList, Id, Text> = [
+    ...TList,
+    {
+        id: Id;
+        text: Text;
+        completed: false;
+    },
+];
+type RemoveItem<TList extends TodoList, Id> = TList extends [
     infer First,
-    ...infer Tail,
+    ...infer Rest,
 ]
-    ? [...Reverse<Tail>, First]
+    ? First extends { id: Id }
+        ? [...(Rest & TodoList)]
+        : First extends { id: infer FirstId }
+          ? [...[First & { id: FirstId }], ...RemoveItem<Rest & TodoList, Id>]
+          : []
     : [];
-type Ten = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-type Res4 = Reverse<Ten>;
-
-// По умолчанию глубина рекурсии 50
-// @ts-expect-error
-type Res5 = Reverse<[...Ten, ...Ten, ...Ten, ...Ten, ...Ten, 1]>;
-
-// На хвостовую рекурсию ограничение 1000
-type CreateTuple<
-    Num extends number,
-    Tuple extends unknown[] = [],
-> = Tuple["length"] extends Num ? Tuple : CreateTuple<Num, [...Tuple, unknown]>;
-
-type Res3 = CreateTuple<999>;
+// type UpdateText<TodoList, Id, Text> =
+// type ToggleCompleted<TodoLIst, Id> =
+// type FindById<TodoList, Id> =
+// type FilterBy<TodoList, Pattern> =
